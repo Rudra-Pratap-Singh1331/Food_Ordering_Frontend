f6215b902f5a1d962962a8733717c719eabf2dce
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-08-01 23:59:30 +0530

    some changes have been done

--- src/data/sahkes.js
+++ src/data/sahkes.js
@@ -4,21 +4,24 @@ const shakesData = [
     name: "The Thick Shake Factory",
     location: "Gomti Nagar",
     image: "https://b.zmtcdn.com/data/pictures/chains/5/18163865/cd1e9b950d489ad142a03788b7db1ab3.jpg?fit=around|960:500&crop=960:500;*,*",
-    rating: 4.2
+    rating: 4.2,
+    costForTwo: "₹400 for two",
   },
   {
     id: 2,
     name: "Frozen Bottle",
     location: "Hazratganj",
     image: "https://frozenbottle.com/cdn/shop/files/Shopify_share_image.png?v=1670910104",
-    rating: 4.1
+    rating: 4.1,
+    costForTwo: "₹350 for two",
   },
   {
     id: 3,
     name: "Baskin Robbins",
     location: "Indira Nagar",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_AkFx13-yw1o_JPpizB1wPcHZDUZRGyyLqg&s",
-    rating: 4.5
+    rating: 4.5,
+    costForTwo: "₹500 for two",
   }
 ];
 
